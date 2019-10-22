56c14778ab074ee37b08b97ee326f08b275ed2a2
Faisal Muhammad <t102855@corp.telenor.no>
2019-10-22 23:12:34 +0500

    commit

--- instances.tf
+++ instances.tf
@@ -17,3 +17,65 @@ resource "aws_instance" "rpa_vdi" {
 
 
 }
+
+
+
+resource "aws_instance" "rpa_orch" {
+  instance_type = "t2.large"
+  ami           = "${var.win_ami}"
+  subnet_id     = "${aws_subnet.rpa-nonexposed.id}"
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "200"
+  }
+  tags = {
+    Name        = "rpa-orch"
+    Envrionment = "poc"
+    Owner       = "gss"
+  }
+  key_name               = "${aws_key_pair.sshkey.id}"
+  vpc_security_group_ids = ["${aws_security_group.sg-nonexposed.id}"]
+
+
+}
+
+
+resource "aws_instance" "rpa_index" {
+  instance_type = "t2.large"
+  ami           = "${var.win_ami}"
+  subnet_id     = "${aws_subnet.rpa-secure.id}"
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "400"
+  }
+  tags = {
+    Name        = "rpa-index"
+    Envrionment = "poc"
+    Owner       = "gss"
+  }
+  key_name               = "${aws_key_pair.sshkey.id}"
+  vpc_security_group_ids = ["${aws_security_group.sg-secure.id}"]
+
+}
+
+
+resource "aws_instance" "rpa_activedir" {
+  instance_type = "t2.large"
+  ami           = "${var.win_ami}"
+  subnet_id     = "${aws_subnet.rpa-secure.id}"
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "400"
+  }
+  tags = {
+    Name        = "rpa-index"
+    Envrionment = "poc"
+    Owner       = "gss"
+  }
+  key_name               = "${aws_key_pair.sshkey.id}"
+  vpc_security_group_ids = ["${aws_security_group.sg-activedir.id}"]
+
+}
--- sg.tf
+++ sg.tf
@@ -9,4 +9,50 @@ resource "aws_security_group" "sg-management" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "sg-nonexposed" {
+  name        = "sg-nonexposed"
+  description = "SG for Nonexposed"
+  vpc_id      = "${var.rpa_vpc_id}"
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "sg-secure" {
+  name        = "sg-secure"
+  description = "SG for Secure"
+  vpc_id      = "${var.rpa_vpc_id}"
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "sg-activedir" {
+  name        = "sg-secure"
+  description = "SG for activedir"
+  vpc_id      = "${var.rpa_vpc_id}"
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
